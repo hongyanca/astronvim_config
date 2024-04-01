8ab81f332f66186d0c32376145d821b67e9fb008
Hong Yan <contact@hongyan.ca>
2024-04-01 15:30:23 -0600

    feat: initial commit

---
+++ lua/community.lua
@@ -0,0 +1,41 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  -- Add the community repository of plugin specifications
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  "AstroNvim/astrocommunity",
+  -- example of importing an entire language pack
+  -- these packs can set up things such as Treesitter, Language Servers,
+  -- additional language specific plugins, and more!
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  -- Importing themes
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.vscode-nvim" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  -- GitHub Copilot
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.copilot-lua" },
+  -- { -- further customize the options set by the community
+  --   "zbirenbaum/copilot.lua",
+  --   opts = {
+  --     suggestion = {
+  --       keymap = {
+  --         accept = "<C-l>",
+  --         accept_word = false,
+  --         accept_line = false,
+  --         next = "<C-.>",
+  --         prev = "<C-,>",
+  --         dismiss = "<C/>",
+  --       },
+  --     },
+  --   },
+  -- },
+}
